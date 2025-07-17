f42662c635454f2aa4d0e5ed0001cf406bd1529e
mavor <ma_voroshilov@mail.ru>
2025-07-17 23:20:19 +0300

    Complete functionality

--- Windows.py
+++ Windows.py
@@ -2,13 +2,20 @@ import datetime
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 
+from Doc import Doc, DocGroup
+
+import locale
+locale.setlocale(
+    category=locale.LC_ALL,
+    locale="Russian"
+)
 
 class GeneralSettigns(QWidget):
-    def __init__(self):
+    def __init__(self, names=None, date=None):
         super().__init__()
-        self.initUI()
+        self.initUI(names, date)
 
-    def initUI(self):
+    def initUI(self, names, date):
         widgetX = 120
         widgetY = 30
 
@@ -23,6 +30,8 @@ class GeneralSettigns(QWidget):
         self.dateEdit = QDateEdit(self)
         self.dateEdit.setGeometry(160, 20, widgetX, widgetY)
         self.dateEdit.setDate(datetime.datetime.now())
+        if date is not None:
+            self.dateEdit.setDate(date)
 
         self.windowLbl = QLabel(self)
         self.windowLbl.setText("Члены комиссии")
@@ -32,6 +41,8 @@ class GeneralSettigns(QWidget):
         self.comissionList = QTextEdit(self)
         self.comissionList.setGeometry(30, 110, 240, 250)
         self.comissionList.setPlaceholderText("Впишите членов комиссии по одному в строке")
+        if names is not None:
+            self.comissionList.setText('\n'.join(names))
 
         self.fileLbl = QLabel(self)
         self.fileLbl.setText("Загрузить файл")
@@ -82,9 +93,9 @@ class ErrorWindow(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Ошибка")
-        self.setFixedSize(300, 70)
+        self.setFixedSize(300, 90)
         self.lbl = QLabel(self)
-        self.lbl.setGeometry(20, 20, 260, 30)
+        self.lbl.setGeometry(20, 20, 260, 50)
         self.lbl.setText(self.text)
         self.lbl.setAlignment(Qt.AlignCenter)
 
@@ -117,6 +128,7 @@ class DocumentSettings(QWidget):
         self.number.setPlaceholderText("Номер")
         self.number.setGeometry(210, 60, smallX, widgetY)
 
+        self.nonPersonReasons = ["представление вице-президента Адвокатской палаты Санкт-Петербурга"]
         self.reason = QComboBox(self)
         self.reason.addItems(["жалоба", "жалоба адвоката",
                               "представление вице-президента Адвокатской палаты Санкт-Петербурга",
@@ -153,11 +165,73 @@ class DocumentSettings(QWidget):
         self.backBtn.clicked.connect(self.back)
 
     def addDoc(self):
-        pass
+        target = self.targetName.toPlainText()
+        reason = self.reason.currentText()
+        number = self.number.toPlainText()
+        initiator = None
+        if reason not in self.nonPersonReasons:
+            initiator = self.initiator.toPlainText()
+        if target == "":
+            self.ew = ErrorWindow("Введите имя адвоката")
+            self.ew.show()
+            return
+        if number == "":
+            self.ew = ErrorWindow("Введите реестровый номер")
+            self.ew.show()
+            return
+        if initiator is not None and initiator == "":
+            self.ew = ErrorWindow("Введите имя инициатора")
+            self.ew.show()
+            return
+        if initiator is None:
+            initiator = ""
+        newRecord = "-".join([target, number, reason, initiator])
+        currentRecords = self.docs.toPlainText()
+        currentRecords += newRecord + "\n"
+        self.docs.setText(currentRecords)
+
 
     def back(self):
-        pass
+        self.gs = GeneralSettigns(names=self.comissionNames, date=self.date)
+        self.gs.show()
+        self.close()
 
     def save(self):
-        pass
-
+        months = {'Январь': 'января', 'Февраль': 'февраля', 'Март': 'марта', 'Апрель': 'апреля', 'Май': 'мая',
+                  'Июнь': 'июня', 'Июль': 'июля', 'Август': 'августа', 'Сентябрь': 'сентября', 'Октябрь': 'октября',
+                  'Ноябрь': 'ноября', 'Декабрь': 'декабря'}
+        try:
+            filename = self.saveName.toPlainText()
+            if filename == "":
+                self.ew = ErrorWindow("Введите имя сохраняемого файла")
+                self.ew.show()
+                return
+            day, month, year = self.date.strftime("%d %B %Y").split()
+            longDate = day + ' ' + months[month] + ' ' + year + " года"
+            docs = DocGroup()
+            records = self.docs.toPlainText().split('\n')
+            records = [record for record in records if record]
+            if len(records) == 0:
+                self.ew = ErrorWindow("Добавьте дела")
+                self.ew.show()
+                return
+            for i in range(len(records)):
+                record = records[i]
+                parts = record.split("-")
+                if len(parts) != 4:
+                    self.ew = ErrorWindow(f"Неверный формат в строке {i + 1}.\nУдалите строку и добавьте данные заново")
+                    self.ew.show()
+                    return
+                target, number, reason, initiator = parts
+                if reason in self.nonPersonReasons and initiator != "":
+                    self.ew = ErrorWindow("Данная причина не требует ФИО. Следуйте инструкциям в верхней части окна")
+                    self.ew.show()
+                    return
+                for voter in self.comissionNames:
+                    docs.addDoc(Doc(longDate, voter, target, number, reason, initiator))
+            docs.save(filename)
+            self.back()
+        except Exception as e:
+            self.ew = ErrorWindow("Ошибка при создании файла.\nУбедитесь, что файл с таким названием \nне существует или закрыт")
+            self.ew.show()
+            return
